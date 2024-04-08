ab5335aa4eec108527a97ac9e1a2ed369bf5176f
Matheus <contato.matheused@gmail.com>
2024-04-08 09:55:36 -0300

    optimization

--- two_sum_problem.py
+++ two_sum_problem.py
@@ -2,10 +2,9 @@ def two_sum(nums, target):
     solution = {}
 
     for index, num in enumerate(nums):
-        solution[target-num] = index
         if solution.get(num) is not None:
             return [solution.get(num), index]
-        
+        solution[target-num] = index
 
 nums = [7, 1, 5, 8]
 target = 12
